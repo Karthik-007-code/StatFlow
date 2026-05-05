f2fa572f4764e2dcd965c6e0c0999c491a64e9ca
karthik <karthikankipalli007@gmail.com>
2026-05-05 22:38:20 +0530

    changed some thing

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -18,7 +18,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-app.use(cors());
+
+
+app.use(cors({
+  origin: 'https://statflow-94h5.onrender.com',
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
