53656e9c15c8340f6e13570754f95fafb20eaf0e
karthik <karthikankipalli007@gmail.com>
2026-05-05 21:50:08 +0530

    fixed some bugs

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -6,6 +6,7 @@ import mongoose from "mongoose";
  */
 const connectDB = async () => {
   try {
+
     const conn = await mongoose.connect(process.env.MONGO_STR, {
       dbName: "statflow",
     });
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -11,7 +11,7 @@ import errorHandler from "./middleware/errorHandler.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 
 const app = express();
